dd431964fb2a3483b8bd71b977e306340e50f31a
samhaqk <samhaqk@gmail.com>
2022-11-30 21:46:02 +0530

    text added

---
+++ app/src/main/java/com/samhaqk/taskmanager/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.samhaqk.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.samhaqk.taskmanager.ui.theme.TaskManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TaskText()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskText() {
+    Column {
+        Text(
+            text = "All tasks completed",
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = "Nice Work!",
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TaskManagerTheme {
+        TaskText()
+    }
+}

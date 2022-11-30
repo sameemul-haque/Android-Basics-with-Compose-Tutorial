bba00745ea8371d3ca6fcbc59b2a1638b391a6c0
samhaqk <samhaqk@gmail.com>
2022-11-30 22:04:23 +0530

    background image added

--- app/src/main/java/com/samhaqk/taskmanager/MainActivity.kt
+++ app/src/main/java/com/samhaqk/taskmanager/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,44 +30,65 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    TaskText()
+                    BgImg()
                 }
             }
         }
     }
-}
 
-@Composable
-fun TaskText() {
-    val checkmark = painterResource(id = R.drawable.checkmark)
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Image(
-            painter = checkmark,
-            contentDescription = null
-        )
-        Text(
-            text = stringResource(R.string.all_tasks_completed),
-            fontSize = 24.sp,
-            modifier = Modifier
-                .padding(top = 24.dp, bottom = 8.dp)
-        )
-        Text(
-            text = stringResource(R.string.nice_work),
-            fontSize = 16.sp
-        )
+    @Composable
+    fun BgImg() {
+        val bg = painterResource(id = R.drawable.bg)
+        Box {
+            Image(
+                painter = bg,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+            TaskText()
+        }
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TaskManagerTheme {
-        TaskText()
+    @Composable
+    fun TaskText() {
+        val checkmark = painterResource(id = R.drawable.checkmark)
+        Surface(
+            color = Color.Black.copy(alpha = 0.3f),
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = checkmark,
+                    contentDescription = null
+                )
+                Text(
+                    text = stringResource(R.string.all_tasks_completed),
+                    fontSize = 24.sp,
+                    modifier = Modifier
+                        .padding(top = 24.dp, bottom = 8.dp)
+                )
+                Text(
+                    text = stringResource(R.string.nice_work),
+                    fontSize = 16.sp
+                )
+            }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        TaskManagerTheme {
+            BgImg()
+        }
     }
 }

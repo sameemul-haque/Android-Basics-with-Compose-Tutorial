e09ba7dc47ca37bb9af809d4c548b58615159ea8
samhaqk <samhaqk@gmail.com>
2022-11-30 21:49:32 +0530

    checkmark image added

--- app/src/main/java/com/samhaqk/taskmanager/MainActivity.kt
+++ app/src/main/java/com/samhaqk/taskmanager/MainActivity.kt
@@ -3,6 +3,7 @@ package com.samhaqk.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,6 +12,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,7 +37,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TaskText() {
+    val checkmark = painterResource(id = R.drawable.checkmark)
     Column {
+        Image(
+            painter = checkmark,
+            contentDescription = null
+        )
         Text(
             text = "All tasks completed",
             fontSize = 24.sp,

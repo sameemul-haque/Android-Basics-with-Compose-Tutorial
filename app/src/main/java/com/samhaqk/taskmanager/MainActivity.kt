08ca5c74e7e87cd36becd1221cc03124c67a8842
samhaqk <samhaqk@gmail.com>
2022-11-30 21:52:55 +0530

    center alignment done

--- app/src/main/java/com/samhaqk/taskmanager/MainActivity.kt
+++ app/src/main/java/com/samhaqk/taskmanager/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,7 +37,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TaskText() {
     val checkmark = painterResource(id = R.drawable.checkmark)
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
         Image(
             painter = checkmark,
             contentDescription = null

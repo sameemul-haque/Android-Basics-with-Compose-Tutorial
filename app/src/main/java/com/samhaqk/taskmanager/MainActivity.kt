d687d4c64ffd01108ed00a9f402d06add75f52ba
samhaqk <samhaqk@gmail.com>
2022-11-30 21:55:46 +0530

    hardcoded string is replaced with a stringResource() function.

--- app/src/main/java/com/samhaqk/taskmanager/MainActivity.kt
+++ app/src/main/java/com/samhaqk/taskmanager/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,13 +50,13 @@ fun TaskText() {
             contentDescription = null
         )
         Text(
-            text = "All tasks completed",
+            text = stringResource(R.string.all_tasks_completed),
             fontSize = 24.sp,
             modifier = Modifier
                 .padding(top = 24.dp, bottom = 8.dp)
         )
         Text(
-            text = "Nice Work!",
+            text = stringResource(R.string.nice_work),
             fontSize = 16.sp
         )
     }

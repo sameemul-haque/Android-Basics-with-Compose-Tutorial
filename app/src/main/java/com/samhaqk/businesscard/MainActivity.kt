5ea1a8505f3b1af89a7ffe36fe70a4a61667d8dc
samhaqk <samhaqk@gmail.com>
2022-12-06 19:01:05 +0530

    added profile pic, name and title

--- app/src/main/java/com/samhaqk/businesscard/MainActivity.kt
+++ app/src/main/java/com/samhaqk/businesscard/MainActivity.kt
@@ -3,13 +3,18 @@ package com.samhaqk.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.samhaqk.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    BusinessCard()
                 }
             }
         }
@@ -30,14 +35,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BusinessCard() {
+    val profile = painterResource(id = R.drawable.profile)
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = profile,
+            contentDescription = "Profile Pic",
+            modifier = Modifier.height(250.dp).width(250.dp)
+        )
+        Text(
+            text = "SamHaqK",
+            fontSize = 24.sp,
+            modifier = Modifier.padding(top = 24.dp, bottom = 24.dp)
+        )
+        Text(
+            text = "Android Developer",
+            fontSize = 16.sp,
+            modifier = Modifier.padding(top = 16.dp, bottom = 16.dp)
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BusinessCardTheme {
-        Greeting("Android")
+
     }
 }
